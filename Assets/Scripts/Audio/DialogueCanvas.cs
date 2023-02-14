c78dbb0637faf9c926d8078e44f0a9038036ae3e
Calle1122 <carl@lindstedt.net>
2023-02-14 16:41:15 +0100

    Objective UI
    Implemented Objective UI and added functionality for future inventory system.

--- Assets/Scripts/Audio/DialogueCanvas.cs
+++ Assets/Scripts/Audio/DialogueCanvas.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Audio
 {
@@ -9,8 +10,11 @@ namespace Audio
     {
         public static DialogueCanvas Instance { get; private set; }
 
-        private TextMeshProUGUI _dialogueTxt;
+        [SerializeField] private TextMeshProUGUI dialogueTxt, objectiveTxt;
+        [SerializeField] private Image ritualItem1Image, ritualItem2Image, ritualItem3Image;
+        [SerializeField] private Sprite ritualItem1Sprite, ritualItem2Sprite, ritualItem3Sprite;
 
+        [Header("Debug Information")]
         [SerializeField] private bool isPlaying;
         private Queue<DialogueSo> _dialogueQueue = new Queue<DialogueSo>();
 
@@ -25,8 +29,7 @@ namespace Audio
                 Instance = this;
             }
 
-            _dialogueTxt = GetComponentInChildren<TextMeshProUGUI>();
-            _dialogueTxt.enabled = false;
+            dialogueTxt.enabled = false;
             isPlaying = false;
         }
 
@@ -49,12 +52,12 @@ namespace Audio
             yield return new WaitForSeconds(.5f);
             
             //Set text and enable it
-            _dialogueTxt.text = dialogueToPlay.dialogueText;
-            _dialogueTxt.enabled = true;
+            dialogueTxt.text = dialogueToPlay.dialogueText;
+            dialogueTxt.enabled = true;
             
             //Wait for dialogue to finish and disable the text
             yield return new WaitForSeconds(dialogueToPlay.dialogueSound.clips[0].length);
-            _dialogueTxt.enabled = false;
+            dialogueTxt.enabled = false;
             
             //If there are more queued... play next!
             if (_dialogueQueue.Count > 0)
@@ -64,6 +67,32 @@ namespace Audio
             else
             {
                 isPlaying = false;
+            }
+        }
+
+        public void UpdateMainObjective(string newObjective)
+        {
+            //TODO: Include crossing off old objective before fading (some animation) to the new objective.
+            
+            objectiveTxt.text = newObjective;
+        }
+        
+        public void UnlockRitualItem(int itemToUnlock)
+        {
+            switch (itemToUnlock)
+            {
+                case 1:
+                    ritualItem1Image.sprite = ritualItem1Sprite;
+                    break;
+                case 2:
+                    ritualItem2Image.sprite = ritualItem2Sprite;
+                    break;
+                case 3:
+                    ritualItem3Image.sprite = ritualItem3Sprite;
+                    break;
+                default:
+                    Debug.LogError("Invalid ritual item index when trying to unlock ritual item UI.", this);
+                    break;
             }
         }
     }
