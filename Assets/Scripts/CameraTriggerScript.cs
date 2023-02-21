438594a928813515820d40a9b566e3f46b6dd121
Calle1122 <carl@lindstedt.net>
2023-02-21 15:17:06 +0100

    Two Player Camera Trigger
    Camera now waits for both players before switching to next room

--- Assets/Scripts/CameraTriggerScript.cs
+++ Assets/Scripts/CameraTriggerScript.cs
@@ -6,11 +6,29 @@ public class CameraTriggerScript : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera triggerCamera;
 
+    private bool _ghostEntered, _humanEntered;
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag(Tags.PlayerTag) || other.CompareTag(Tags.GhostTag))
+        if (other.CompareTag(Tags.PlayerTag))
         {
-            CameraManagerScript.CurrentActiveCamera = triggerCamera;
+            _humanEntered = true;
         }
+        if (other.CompareTag(Tags.GhostTag))
+        {
+            _ghostEntered = true;
+        }
+
+        if (_humanEntered && _ghostEntered)
+        {
+            SetActiveCamera();
+        }
+    }
+
+    private void SetActiveCamera()
+    {
+        CameraManagerScript.CurrentActiveCamera = triggerCamera;
+        _ghostEntered = false;
+        _humanEntered = false;
     }
 }
