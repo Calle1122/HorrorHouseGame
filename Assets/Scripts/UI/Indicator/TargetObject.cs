39321fb98e1726bb4b9300eb6c7577b42322ff4b
Calle1122 <carl@lindstedt.net>
2023-02-21 17:26:57 +0100

    Target Indicators

---
+++ Assets/Scripts/UI/Indicator/TargetObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetObject : MonoBehaviour
+{
+    public enum Profile
+    {
+        Human,
+        Ghost
+    }
+
+    public Profile thisProfile;
+    
+    private void Awake()
+    {
+        IndicatorController indicator = GetComponentInParent<IndicatorController>();
+        if(indicator == null)
+        {
+            indicator = GameObject.Find("IndicatorController").GetComponent<IndicatorController>();
+        }
+
+        if (indicator == null) Debug.LogError("No UIController component found");
+
+        indicator.AddTargetIndicator(this.gameObject);
+    }
+}
