e6958350fb73f4b4739cc0b3bbdafc02984ac64f
Calle1122 <carl@lindstedt.net>
2023-02-22 12:16:36 +0100

    Indicator Fix & Camera Check
    Fixed the requests for the indicator code and fixed a bug with the camera triggers.

--- Assets/Scripts/UI/Indicator/IndicatorController.cs
+++ Assets/Scripts/UI/Indicator/IndicatorController.cs
@@ -1,25 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class IndicatorController : MonoBehaviour
 {
     public Canvas canvas;
-
     public List<TargetIndicator> targetIndicators = new List<TargetIndicator>();
-
     public Camera MainCamera;
-
     public GameObject TargetIndicatorPrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if(targetIndicators.Count > 0)
