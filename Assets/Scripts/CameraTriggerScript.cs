15bf5143e11cdfa99b1853fe7ac66cfdd34839fd
Calle1122 <carl@lindstedt.net>
2023-02-23 13:02:19 +0100

    Camera Fix

--- Assets/Scripts/CameraTriggerScript.cs
+++ Assets/Scripts/CameraTriggerScript.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Cinemachine;
 using GameConstants;
 using UnityEngine;
@@ -6,6 +7,11 @@ public class CameraTriggerScript : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera triggerCamera;
 
+    [SerializeField] private bool isMainRoomTrigger;
+    [SerializeField] private List<CameraTriggerScript> roomsToReset;
+
+    [HideInInspector]public bool hasDoneMainTransition;
+    
     private bool _ghostEntered, _humanEntered;
 
     private void OnTriggerEnter(Collider other)
@@ -28,6 +34,11 @@ public class CameraTriggerScript : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
+        if (isMainRoomTrigger && !hasDoneMainTransition)
+        {
+            return;
+        }
+        
         if (other.CompareTag(Tags.PlayerTag))
         {
             _humanEntered = false;
@@ -44,5 +55,17 @@ public class CameraTriggerScript : MonoBehaviour
         CameraManagerScript.CurrentActiveCamera = triggerCamera;
         _ghostEntered = false;
         _humanEntered = false;
+
+        if (!isMainRoomTrigger || hasDoneMainTransition)
+        {
+            return;
+        }
+
+        hasDoneMainTransition = true;
+        
+        foreach (CameraTriggerScript camTrigger in roomsToReset)
+        {
+            camTrigger.hasDoneMainTransition = false;
+        }
     }
 }
